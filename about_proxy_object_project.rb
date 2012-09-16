eee9315c74b675b115b9d51b05e7d5f2a6e27ae5
Hassan Shamim <hassan.has.you@gmail.com>
2012-09-16 14:46:23 -0500

    eat it rubykoans!

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -15,9 +15,21 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 class Proxy
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
   end
-
+  def messages
+    @messages
+  end
+  def number_of_times_called(method_name)
+    @messages.count(method_name)
+  end
+  def called?(method_name)
+    @messages.include?(method_name)
+  end
+  def method_missing(method_name, *args, &block)
+    @messages << method_name.intern
+    @object.send(method_name, *args, &block)
+  end
   # WRITE CODE HERE
 end
 
